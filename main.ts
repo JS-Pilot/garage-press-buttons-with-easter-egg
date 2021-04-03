f872cb4664502ad07d80025161bf0d03d0cc0243
Johnny <76563520+JS-Pilot@users.noreply.github.com>
2021-04-03 13:43:53 +0300

    Workaround on array equality conditional, Fix RIGHT order

--- main.ts
+++ main.ts
@@ -1,9 +1,6 @@
 input.onButtonPressed(Button.A, function () {
     A_NUMBER += 1
     LIST.unshift(A_NUMBER)
-    if (0 >= 0) {
-        A_NUMBER = 0
-    }
 })
 input.onButtonPressed(Button.AB, function () {
     basic.showString("" + (A_NUMBER))
@@ -13,25 +10,34 @@ input.onButtonPressed(Button.AB, function () {
 input.onButtonPressed(Button.B, function () {
     B_NUMBER += 1
     LIST.unshift(B_NUMBER)
-    if (B_NUMBER >= 0) {
-        B_NUMBER = 0
-    }
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     basic.showString("" + (B_NUMBER))
     B_NUMBER = 0
     basic.clearScreen()
 })
+let index_array = 0
+let entered_code_is_correct = false
+let B_NUMBER = 0
+let A_NUMBER = 0
 let LIST: number[] = []
-let B_NUMBER: number = []
-let A_NUMBER: number = []
+LIST = [0]
 A_NUMBER = 0
 B_NUMBER = 0
-LIST = [0]
-let RIGHT = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+let RIGHT = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
 basic.forever(function () {
-    if (LIST.length != RIGHT.length) {
-        if (LIST == RIGHT) {
+    // Array equality conditional not working in microbit
+    if (LIST.length == RIGHT.length) {
+        entered_code_is_correct = true
+        // scan array for equality
+        index_array = 0
+        while (index_array < RIGHT.length) {
+            if (LIST[index_array] != RIGHT[index_array]) {
+                entered_code_is_correct = false
+            }
+            index_array += 1
+        }
+        if (entered_code_is_correct == true) {
             music.playMelody("C D E F G A B C5 ", 120)
             LIST = [0]
         }
